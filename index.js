81166d518ed4ce4ef3ad8f5386b7d446310ac907
123naim <mdnayemiqbal886@gmail.com>
2024-05-28 12:30:36 +0600

    deffarent branch command added to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// to Check branch-- command: git branch
+// to create new brance-- command: git checkout <-new branch name->
+// to rename branch-- command: git branch -M main
+// to nevigate-- command: git checkout <-branch name->
+// to delete branch-- command: git branch -d <-branch name->
